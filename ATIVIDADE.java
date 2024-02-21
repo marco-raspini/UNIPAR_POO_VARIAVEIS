8cd86cbf35887ce511ed6decf05cfcf61e4bbe4f
Marco Antonio Raspini da Silva <72366900+marco-raspini@users.noreply.github.com>
2024-02-20 22:23:46 -0300

    Create ATIVIDADE.java
    Primeiro commit da atividade

---
+++ ATIVIDADE.java
@@ -0,0 +1,40 @@
+
+// ATIVIDADES 01
+
+class Atividade {
+    public static void main(String[] args) {
+    
+    String nomeProduto = "Camiseta";
+    Double precoProduto = 349.90;
+    String marcaDoProduto = "Nike";
+    Integer quantidadeProdutoEstoque = 50;
+    String descricaoProduto = "Camiseta da Nike Original";
+    char tamanhoProduto = 'M';
+    boolean produtoExclusivoPromocional = true;
+    short descontoPromocional = 25; 
+    String localArmazenado = "Cidade x";
+    long cepEnvioDestino = 85603780;
+    
+    }
+}
+
+// ATIVIDADE 02
+
+class Atividade {
+    public static void main(String[] args) {
+    
+        String nomeProduto;
+        Double precoProduto;
+        String marcaDoProduto;
+        Integer quantidadeProduto;
+        String descricaoProduto;
+        String nomeProdutoTeste;
+        String numeroProduto;
+        double precoDoProduto; 
+        long estoqueDisponivel;
+        Integer totalDeProdutos;
+    }
+}
+
+// ATIVIDADE 03
+
