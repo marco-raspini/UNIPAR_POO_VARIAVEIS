31f5422c369806f77f5d9f44eee2bb66337e4b9f
Marco Antonio Raspini da Silva <72366900+marco-raspini@users.noreply.github.com>
2024-03-04 22:00:38 -0300

    Update ATIVIDADE03.java

--- ATIVIDADE03.java
+++ ATIVIDADE03.java
@@ -1,1 +1,39 @@
 
+// ATIVIDADE 01
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        int notaUm;
+        int notaDois;
+        int notaTres;
+        int media;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Informe suas notas:  ");
+        notaUm = scanner.nextInt();
+        notaDois = scanner.nextInt();
+        notaTres = scanner.nextInt();
+        
+        scanner.close();
+        
+        System.out.println("Nota 1 - "+ notaUm);
+        System.out.println("Nota 2 - "+ notaDois);
+        System.out.println("Nota 3 - "+ notaTres);
+        
+        media = (notaUm + notaDois + notaTres);
+
+        System.out.println("");
+        System.out.println("Media Final: " + media);
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
