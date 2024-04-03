f1103292dc863e50c24a2860eedd573a0958d6c4
Marco Antonio Raspini da Silva <72366900+marco-raspini@users.noreply.github.com>
2024-04-02 22:42:02 -0300

    Update ATIVIDADE05.java

--- ATIVIDADE05.java
+++ ATIVIDADE05.java
@@ -22,7 +22,7 @@ class ProgramaFizzBuzz {
 
 import java.util.Scanner;
 
-class HelloWorld {
+class PatinhosDaXuxa {
     public static void main(String[] args) {
        
         int patinhos;
@@ -92,3 +92,41 @@ class ProgramaIdadeEmDias {
 
 // ATIVIDADE 04
 
+import java.util.Scanner;
+
+//Faaa um algoritmo que leia 20 numeros e, ao final, escreva quantos estao entre 0 e
+//100, quantos estao entre 101 e 200 e quantos sao maiores de 200
+
+class ContaNumeros {
+    public static void main(String[] args) {
+       
+        int numeros;
+        int numero;
+        int maiorZero = 0;
+        int maiorCem = 0;
+        int maiorDuz = 0;
+        int i = 0;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        while (i <= 4) {
+            i++;
+            
+            System.out.println("Informe o numero:  ");
+            numero = scanner.nextInt();
+            System.out.println();
+            
+            if ((numero >= 0) && (numero <= 100)){
+                maiorZero++;
+            } else if ((numero >= 101) && (numero <= 200)){
+                maiorCem++;
+            } else if ((numero > 200){
+                maiorDuz++;
+            }
+        }
+        scanner.close();
+    }
+    System.out.println(maiorZero);
+    System.out.println(maiorCem);
+    System.out.println(maiorDuz);
+}
