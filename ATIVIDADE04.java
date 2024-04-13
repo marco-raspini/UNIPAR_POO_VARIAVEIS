6ff737f6332afc1964c049019c4721b6ec3e6c15
Marco Antonio Raspini da Silva <72366900+marco-raspini@users.noreply.github.com>
2024-04-13 00:32:30 -0300

    Update ATIVIDADE04.java

--- ATIVIDADE04.java
+++ ATIVIDADE04.java
@@ -28,6 +28,97 @@ class HelloWorld {
 
 // ATIVIDADE 02
 
+import java.util.Scanner;
 
+class IdentificaParImpar {
+    public static void main(String[] args) {
+      
+        int numero;
+        int identificador;
+      
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Informe o numero inteiro:  ");
+        numero = scanner.nextInt();
+        System.out.println();
+        
+        scanner.close();
+        
+        identificador = (numero % 2);
+        
+        if (identificador == 0) {
+            System.out.println("Seu numero e Par");
+        }
+        else {
+            System.out.println("Seu numero e Impar");
+        }
+    }
+}
 
 //------------------------------------------------------------------------------------------------------------------
+
+// ATIVIDADE 03
+
+import java.util.Scanner;
+
+class IdentificaParImpar {
+    public static void main(String[] args) {
+
+        String loginCorreto = "usuario";
+        String senhaCorreta = "senha123";
+
+        String login;
+        String senha;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o Login:  ");
+        login = scanner.next();
+        System.out.println("Informe a Senha: ");
+        senha = scanner.next();
+        System.out.println();
+
+        scanner.close();
+
+        if ((login.equals(loginCorreto)) && (senha.equals(senhaCorreta)) ){
+            System.out.println("Login realizado com sucesso");
+        }
+        else {
+            System.out.println("Nome de usuario ou senha incorretos");
+        }
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------
+
+// ATIVIDADE 04
+
+import java.util.Scanner;
+
+class IdentificaParImpar {
+    public static void main(String[] args) {
+      
+        int numFirst;
+        int numSecond;
+      
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Informe o primeiro numero:  ");
+        numFirst = scanner.nextInt();
+        System.out.println("Informe o segundo numero:  ");
+        numSecond = scanner.nextInt();
+        System.out.println();
+        
+        scanner.close();
+        
+        
+        if (numFirst > numSecond) {
+            System.out.println("O primeiro numero " + numFirst + " e o Maior que o segundo " + numSecond);
+        }
+        else {
+            System.out.println("O segundo numero " + numSecond + " e o Maior que o primeiro " + numFirst);
+        }
+    }
+}
+
+
