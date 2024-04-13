fe08ca0cd11e08c2293925be6b5e136a5fed3dc9
Marco Antonio Raspini da Silva <72366900+marco-raspini@users.noreply.github.com>
2024-04-12 23:37:45 -0300

    Create ATIVIDADE04.java

---
+++ ATIVIDADE04.java
@@ -0,0 +1,33 @@
+
+// ATIVIDADE 01
+
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+
+        int idadePessoa = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe a sua idade: ");
+        idadePessoa = scanner.nextInt();
+
+        scanner.close();
+        System.out.println("");
+
+        if (idadePessoa >= 18) {
+            System.out.println("Você é maior de idade");
+        } else {
+            System.out.println("Você é menor de idade");
+        }
+        System.out.println("");
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------
+
+// ATIVIDADE 02
+
+
+
+//------------------------------------------------------------------------------------------------------------------
